6fe71ab61e2c623aa2079912d7440aed7f833482
Echenko <119807032+echenko@users.noreply.github.com>
2024-04-20 18:15:42 +0300

    стилизация главной страницы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
 /* в этот файл добавляет скрипты*/
+// решение в лоб... научусь переделаю...
+const menuList = document.querySelector('.navigation__list');
+const menuButton = document.querySelector('.page-header__button');
+
+menuList.classList.remove('navigation__list--no-js');
+
+menuButton.addEventListener('click', () => {
+  menuList.classList.toggle('navigation__list--closed');
+});
