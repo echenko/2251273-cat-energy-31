566278537c6ffd0e66d7228e1c2dc372d5a6e842
Echenko <119807032+echenko@users.noreply.github.com>
2024-04-21 13:16:22 +0300

    начинает стилизацию каталога

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,6 +4,7 @@ const menuList = document.querySelector('.navigation__list');
 const menuButton = document.querySelector('.page-header__button');
 
 menuList.classList.remove('navigation__list--no-js');
+menuButton.classList.remove('page-header__button--no-js');
 
 menuButton.addEventListener('click', () => {
   menuList.classList.toggle('navigation__list--closed');
